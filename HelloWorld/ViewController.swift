bf0c61d43203783a6b524a98c6daf92f6876f8e3
Jesus Gomez <gomezja@Jesuss-MacBook-Pro.local>
2017-06-20 16:59:40 -0700

    completed hello world project

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -14,12 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var welcomeButton: UIButton!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomePressed(_ sender: Any) {
+        welcomeButton.isHidden = true
+        backgroundImage.isHidden = false
+    }
 
 }
 
